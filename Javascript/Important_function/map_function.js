abbde1e9b40a4c3d874470ce3b190cf85559c99f
ashik-islam <ashiksayed8@gmail.com>
2023-03-14 19:21:26 +0600

    map function added

---
+++ Javascript/Important_function/map_function.js
@@ -0,0 +1,22 @@
+/*Source:
+// 1. https://www.freecodecamp.org/news/javascript-map-how-to-use-the-js-map-function-array-method/
+*/
+
+// let someArrayValue = [1,2,3,6];
+// someArrayValue . map
+//   (
+//     value =>
+//     {
+//         let newValue = value + 1;
+//         console.log("This is value:")
+//         document.write(`<h1>${newValue} </h1>`);
+//     } 
+//     );
+
+arr = [3,4,5,4];
+
+for(let i = 0; i<length; i++) {
+    console.log(`${i}:`);
+    
+
+}
