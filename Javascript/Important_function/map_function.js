229a8ae80d88eac0c07329ef0e3e27aecc7b532d
ashik-islam <ashiksayed8@gmail.com>
2023-03-14 22:54:08 +0600

    addedd map function and array function using with map within arry of Object

--- Javascript/Important_function/map_function.js
+++ Javascript/Important_function/map_function.js
@@ -1,7 +1,8 @@
 /*Source:
 // 1. https://www.freecodecamp.org/news/javascript-map-how-to-use-the-js-map-function-array-method/
+   2. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
 */
-
+//JavaScript Map – How to Use the JS .map() Function (Array Method)
 // let someArrayValue = [1,2,3,6];
 // someArrayValue . map
 //   (
@@ -12,6 +13,8 @@
 //         document.write(`<h1>${newValue} </h1>`);
 //     } 
 //     );
+
+
 
 
 // arr = [3,4,5,4];
@@ -41,11 +44,62 @@
 
 //Using array function ======
 
-const arr = [3,5,6,7,8];
+/* const arr = [3,5,6,7,8];
 
 const sqrValue = arr.map((currentValue)=>{
     return Math.pow(currentValue,2);
 });
 
-console.log(sqrValue);
+console.log(sqrValue); */
 
+//Array of Object in  map function =====
+
+/* let nameMan =[
+    {firstName: "Ashik", lastName: "Islam"},
+    {firstName: "Atik", lastName: "Islam"},
+    {firstName: "Anis", lastName: "Rahman"}
+    
+];
+
+fullName = []
+
+for(let i=0; i<nameMan.length; i++){
+    //console.log(`${nameMan[i].firstName} ${nameMan[i].lastName}}`);
+    fullName[i] = `${nameMan[i].firstName} ${nameMan[i].lastName}`;
+    //fullName.push(`${nameMan[i].firstName} ${nameMan[i].lastName}`);
+}
+
+console.log(fullName);
+ */
+
+//===Map function used====
+/* 
+let nameMan =[
+    {firstName: "Ashik", lastName: "Islam"},
+    {firstName: "Atik", lastName: "Islam"},
+    {firstName: "Anis", lastName: "Rahman"}
+    
+];
+
+let fullName = nameMan.map(function(element){
+    return element.firstName. + ' ' + element.lastName;
+});
+
+console.log(fullName); */
+
+
+// Arrow Function using map within array of Object====
+
+let nameMan =[
+    {firstName: "Ashik", lastName: "Islam"},
+    {firstName: "Atik", lastName: "Islam"},
+    {firstName: "Anis", lastName: "Rahman"}
+    
+];
+
+/* let fullName = nameMan.map((element) => {
+    return `${element.firstName.toLowerCase()} ${element.lastName.toLowerCase()}`;
+}); */
+let fullName = nameMan.map((element) => `${element.firstName.toLowerCase()} ${element.lastName.toLowerCase()}`); 
+
+console.log(fullName);
