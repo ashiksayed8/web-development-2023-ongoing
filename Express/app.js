2b6c36965e3576d14dd5bcd430494ea63d038c8e
ashik-islam <ashiksayed8@gmail.com>
2023-03-16 04:54:53 +0600

    Express redrict json cookie header value

--- Express/app.js
+++ Express/app.js
@@ -1,11 +1,34 @@
+const { json } = require('express');
 const express = require('express');
 const app = express();
 const userRouter = require("./routers/users.route");
 
 app.use("/api/user", userRouter);
 
+app.use("/register", (req, res)=> {
+   /*  res.status(200).json({
+        "name" : "Ashikul Islam",
+        "message": "I am register page",
+        statusCode: 200
+    }); */
+
+    // res.redirect("/login");
+    res.statusCode = 200;
+    res.sendFile(__dirname + "/views/register.html");
+});
+
+app.use("/login", (req, res)=> {
+    res.cookie("name" , "Ashik" );
+    res.cookie("age", "30");
+    res.clearCookie("name");
+    res.append("id", "1223");
+    res.end();
+});
+
 app.get('/', (req, res) => {
-    res.send("Ashik");
+    res.statusCode = 200;
+    res.sendFile(__dirname + "/views/index.html");
+  
 });
 
 app.use((req ,res)=> {
